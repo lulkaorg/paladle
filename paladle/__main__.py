2b3d08254fd74c8a8cc4e0e898e8e39b765c2dad
lulkaorg <ryzen55600@proton.me>
2025-07-16 22:32:25 +0200

    test to be able to run the script from everywhere

---
+++ paladle/__main__.py
@@ -0,0 +1,4 @@
+from .paladle import main
+
+if __name__ == '__main__':
+    main()
---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name='paladle',
+    packages=find_packages(),
+    install_requires=requirements,
+    entry_points={
+        'console_scripts': [
+            'paladle=paladle.paladle:main',
+        ],
+    },
+)
