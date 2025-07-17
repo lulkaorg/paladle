94b1d3487f68bfd7928595fba32330472edd137e
lulkaorg <ryzen55600@proton.me>
2025-07-17 21:53:29 +0200

    updated setup.py

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ with open('requirements.txt') as f:
 setup(
     name='paladle',
     packages=find_packages(),
+    include_package_data=True,
     install_requires=requirements,
     entry_points={
         'console_scripts': [
